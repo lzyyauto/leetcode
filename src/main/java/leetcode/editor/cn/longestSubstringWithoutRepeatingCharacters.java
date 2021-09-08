03d11711192ea10b0ab00e3e46859119ed7d32e8
liuziyi <liuziyi@jingdata.com>
2021-09-08 15:11:31 +0800

    003 无重复字符的最长子串 (滑动窗口)

---
+++ src/main/java/leetcode/editor/cn/longestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,88 @@
+//给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: s = "abcabcbb"
+//输出: 3 
+//解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: s = "bbbbb"
+//输出: 1
+//解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: s = "pwwkew"
+//输出: 3
+//解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+//     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+// 
+//
+// 示例 4: 
+//
+// 
+//输入: s = ""
+//输出: 0
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= s.length <= 5 * 10⁴ 
+// s 由英文字母、数字、符号和空格组成 
+// 
+// Related Topics 哈希表 字符串 滑动窗口 👍 6070 👎 0
+
+package leetcode.editor.cn;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+class longestSubstringWithoutRepeatingCharacters {
+    //2021-09-07 17:41:44
+    //无重复字符的最长子串
+    //编号：[3]
+
+    public static void main(String[] args) {
+        Solution solution = new longestSubstringWithoutRepeatingCharacters().new Solution();
+        // TO TEST
+        String s = "dvdf";
+        solution.lengthOfLongestSubstring(s);
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int lengthOfLongestSubstring(String s) {
+            char[] ss = s.toCharArray();
+            int result = 0;
+            int left = 0;
+            int right = 0;
+            LinkedList<Character> record = new LinkedList<>();
+            for (int i = 0; i < ss.length; i++) {
+                while (record.contains(ss[i])) {
+                    record.removeFirst();
+                    left += 1;
+
+                }
+                record.addLast(ss[i]);
+                right += 1;
+                result = (right - left) > result ? (right - left) : result;
+            }
+            return result;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
